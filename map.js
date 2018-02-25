403563f8fa4c10859c1bfe08a792ab3c7ec12b21
Mark Groner <markgroner@utexas.edu>
2018-02-24 20:11:56 -0600

    deleted unnecessary files and added bubbles to map

--- map.js
+++ map.js
@@ -1,10 +1,43 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-
 // Creating our initial map object
 var accessToken = "pk.eyJ1IjoibWFya2dyb25lciIsImEiOiJjamRoazFtN3AwdzRyMndwZ2dxN3hhaWt0In0.Z1mh0XHT_bO7FNgy2ndWLQ";
 var graphCenter = [37.09, -95.71];
 
+
+function createMapLayer(mapStyle, token) {
+  var baseUrl = "https://api.mapbox.com/styles/v1/mapbox/";
+  var layerUrl = `${baseUrl}${mapStyle}/tiles/256/{z}/{x}/{y}?access_token=${token}`
+  console.log(layerUrl)
+  var mapLayer = L.tileLayer(layerUrl);
+  return(mapLayer);
+}
+
+
+// Create a map object
+var myMap = L.map("map", {
+  center: graphCenter,
+  zoom: 7
+});
+
+
+var darkmap = createMapLayer("dark-v9", accessToken);
+// Add a tile layer
+darkmap.addTo(myMap);
+
+// Define a markerSize function that will give each city a different radius based on its population
+function markerSize(magnitude) {
+  return magnitude * 25000;
+}
+
+function getColor(magnitude) {
+    return magnitude >= 8  ? '#BD0026' :
+           magnitude >= 7  ? '#E31A1C' :
+           magnitude >= 6   ? '#FC4E2A' :
+           magnitude >= 5.5   ? '#FD8D3C' :
+           magnitude >= 2.5   ? '#FEB24C' :
+                      '#FED976';
+}
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -12,61 +45,20 @@ d3.json(queryUrl, function(data) {
   createFeatures(data.features);
 });
 
-function createFeatures(earthquakeData) {
+function createFeatures(featureData) {
+    featureData.forEach(function(feature) {
+      var latLong = feature.geometry.coordinates.slice(0,2).reverse();
+      var dateStamp = Date(feature.properties.time);
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
+      var circle = L.circle(latLong, {
+        radius: markerSize(feature.properties.mag),
+        color: "black",
+        fillColor: getColor(feature.properties.mag),
+        fillOpacity: 0.75
+      }).addTo(myMap)
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
-
-
-function createMapLayerURL(mapStyle, token) {
-  var base_url = "https://api.mapbox.com/styles/v1/mapbox/";
-  var layer_url = `${base_url}${mapStyle}/tiles/256/{z}/{x}/{y}?access_token=${token}`
-  var mapLayer = L.tileLayer(layer_url);
-  return(mapLayer);
-}
-function createMap(earthquakes) {
-
-  // Define streetmap and darkmap layers
-  var streetmap = createMapLayerURL("outdoors-v10", accessToken);
-
-  var darkmap = createMapLayerURL("dark-v9", accessToken);
-
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
-
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: graphCenter,
-    zoom: 5,
-    layers: [streetmap, earthquakes]
-  });
-
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-}
+      circle.bindPopup(`<h1> ${feature.properties.place} </h1> <hr>
+        <h3> Date: ${dateStamp} <\h3>
+        <h3> magnitude: ${feature.properties.mag} <\h3>`);
+  })
+};
